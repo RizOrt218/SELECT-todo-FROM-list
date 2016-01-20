1cb9a4e97e5c6c822a83340555f3f4135e45236b
RizOrt218 <rizhiafalen@gmail.com>
2016-01-19 20:17:42 -1000

    alter table tast and droped column completed

--- todo_app.sql
+++ todo_app.sql
@@ -8,13 +8,15 @@ CREATE DATABASE todo_app;
 
 \c todo_app;
 
+DROP TABLE IF EXISTS tasks;
+
 CREATE TABLE tasks (
   id integer NOT NULL PRIMARY KEY,
   title character varying(255) NOT NULL,
   description text NULL,
-  created_at timestamp without time zone NOT NULL now(),
+  created_at timestamp without time zone NOT NULL DEFAULT now(),
   updated_at timestamp without time zone NULL,
-  completed boolean NOT NULL false
+  completed boolean NOT NULL DEFAULT false
   );
 
--- ALTER TABLE tasks DROP COLUMN completed;
+ALTER TABLE tasks DROP COLUMN completed;
