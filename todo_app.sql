1d5722fbdb0bb7248c3fa780b60049325497544a
RizOrt218 <rizhiafalen@gmail.com>
2016-01-19 20:10:25 -1000

    creating table

--- todo_app.sql
+++ todo_app.sql
@@ -7,3 +7,14 @@ DROP DATABASE IF EXISTS todo_app;
 CREATE DATABASE todo_app;
 
 \c todo_app;
+
+CREATE TABLE tasks (
+  id integer NOT NULL PRIMARY KEY,
+  title character varying(255) NOT NULL,
+  description text NULL,
+  created_at timestamp without time zone NOT NULL now(),
+  updated_at timestamp without time zone NULL,
+  completed boolean NOT NULL false
+  );
+
+-- ALTER TABLE tasks DROP COLUMN completed;
